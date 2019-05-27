a289d7dae99827bb34654fab9cf3772e4ce68640
salston77 <salston77@gmail.com>
2019-05-27 15:35:28 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var today = new Date();
+var date = today.getMonth() + 1 + '-' +today.getDay() + '-' + today.getFullYear();
+var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+var dateTime = date+' '+time;
