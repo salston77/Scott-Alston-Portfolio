746351c4fa0d16f9c13eea381f8aeb763b1ba39d
salston77 <salston77@gmail.com>
2019-06-11 19:57:59 -0400

    added text

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ var today = new Date();
 var date = today.getMonth() + 1 + '-' +today.getDay() + '-' + today.getFullYear();
 var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 var dateTime = date+' '+time;
+
+
+window.onload = function() {
+    document.getElementById("fadeIn").style.opacity = 1;
+    document.getElementById("fadeInLonger").style.opacity = 1;
+}
+
+
