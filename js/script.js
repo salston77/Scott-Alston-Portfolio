36818b76abcb5b7d63184c0f0f8f4b488bdef296
salston77 <salston77@gmail.com>
2019-06-18 19:53:14 -0400

    got scroll-spy to work

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ var date = today.getMonth() + 1 + '-' +today.getDay() + '-' + today.getFullYear(
 var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 var dateTime = date+' '+time;
 
+function DateTime() {
+    document.write(dateTime());
+}
+
 
 window.onload = function() {
     document.getElementById("fadeIn").style.opacity = 1;
