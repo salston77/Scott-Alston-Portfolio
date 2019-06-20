3fe65eeb52b807b2227b06ef80bcfce3c78e0337
salston77 <salston77@gmail.com>
2019-06-19 20:09:23 -0400

    add javascript for date and time in footer

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-var today = new Date();
-var date = today.getMonth() + 1 + '-' +today.getDay() + '-' + today.getFullYear();
-var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-var dateTime = date+' '+time;
 
-function DateTime() {
-    document.write(dateTime());
+function dateTime() {
+    var today = new Date();
+    var date = today.getMonth() + 1 + '-' +today.getDay() + '-' + today.getFullYear();
+    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    var dateTime = date+' '+time; 
+    return dateTime;
 }
+
+
 
 
 window.onload = function() {
