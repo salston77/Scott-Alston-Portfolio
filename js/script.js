1f8d566472c6818151f79ab82e7382d6e3878e6a
salston77 <salston77@gmail.com>
2019-07-23 21:06:15 -0400

    fixed date function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 /* JavaScript function to get the current time. Displayed in footer on html page  Hope to be able to use this to check when people visit page
 That functionality is to come if I continue with this site.*/
 function dateTime() {
-    var now = new Date();
-    var date = now.getMonth() + 1 +'-' + now.getDay() +'-' + now.getFullYear();
-    var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-    var date_Time= date+' '+time; 
-    document.write(date_Time);
+    var time = new Date();
+    var date = time.getDate();
+    var month = time.getMonth();
+    var year = time.getFullYear();
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var second = time.getSeconds();
+    var temp = '' + ((hour > 12) ? hour - 12 : hour);
+    if (hour == 0)
+        temp = '12';
+    temp += ((minute < 10) ? ':0' : ':') + minute;
+    temp += ((second < 10) ? ':0' : ':') + second;
+    temp += (hour >= 12) ? ' P.M.' : ' A.M.';
+    temp += ' '+(month + 1)+ '-' + (date)  + '-' +(year);
+    document.write(temp);
 }
 
 
